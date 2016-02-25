4d518adf59e7b34a23b3af036cb26a65b9d2864e
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-02-25 14:37:27 +0100

    Put dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='comet-sensor',
-    version='0.7.2',
+    version='0.7.2.2',
     author = "Rovanion Luckey",
     author_email = "rovanion.luckey@gmail.com",
     description = ("A simple tool for retreiving and dealing with climate data from the Comet T6540 Climate sensor."),
@@ -12,5 +12,10 @@ setup(
         [console_scripts]
         comet-sensor=comet.main:cli
     ''',
-     include_package_data = True
+    include_package_data = True,
+    install_requires=[
+        "click",
+        "plotly",
+        "numpy"
+    ]
 )
